111b6f667447546f0af36f2c3a24c33ae004bcb4
Vinicius Oliveira <vinicius.oliveira148571@gmail.com>
2020-06-26 16:32:55 -0300

    Criando Rotas

---
+++ backend/src/routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const routes = express.Router();
+
+const SessionController = require('./controllers/SessionController');
+const RegisterController = require('./controllers/RegisterController');
+const PostController = require('./controllers/PostController');
+
+routes.post('/sessions', SessionController.create);
+routes.post('/register', RegisterController.create);
+routes.post('/post', PostController.crete);
+
+
+   
+module.exports = routes;
