9e013c60a03c95f7f64480be2f266addd0b25e45
Vinicius Oliveira <vinicius.oliveira148571@gmail.com>
2020-06-27 00:24:48 -0300

    BD Config

---
+++ backend/src/config/database.js
@@ -0,0 +1,17 @@
+module.exports = {
+  dialect: 'mysql',
+  host: "localhost",
+  username: 'root',
+  password: '',
+  database: 'dbtcc',
+  port: 3308,
+define: {
+
+  freezeTableName: true,
+  timestamps: false,
+
+  },
+};
+
+
+
