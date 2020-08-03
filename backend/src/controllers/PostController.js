537913d0b4d5d5b1d65623ea6124e7cd8bc815ab
Vinicius Oliveira <vinicius.oliveira148571@gmail.com>
2020-08-03 15:08:40 -0300

    criação de anúncio

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -1,14 +1,46 @@
-const post = require("../models/post");
+const Post = require("../models/Post.js");
 
 module.exports = {
      async crete(request, response){
-      const{categoria, nome, dsanuncio, tipo, valor} = request.body;
+      const{categoria, nome, dsanuncio, valor, imagem} = request.body;
 
       const cd_usuario = request.headers.authorization;
+          try{
+         await Post.create({cd_anunciante: cd_usuario, cd_categoria: categoria, nm_anuncio: nome,
+          ds_anuncio: dsanuncio, vl_anuncio: valor, nm_img: imagem})
+          .then(newpost => {
+          return response.status(200).json({msg: "Anúncio Realizado com Sucesso", id_post: newpost.cd_anuncio})
+          });
+          }catch(err){
+          return response.status(500).json({msg: "Falha ao realizar o anúncio, por favor tente novamente"})
+          }
+     },
+     async delete(request, response){
+          const id = request.headers.advertiser;
+          
+          try{
+            await Post.destroy({
+            where: {
+              cd_usuario: id
+            }
+          })
+          return response.status(200).json({msg: "Post excluído com sucesso"});
+          } catch(err){
 
-         const {msg, status} = await post.Create(cd_usuario, categoria, nome, dsanuncio, tipo, valor);
-         return response.status(status).json({msg: msg});  
-     }
-}
+          }
+        }
+      }
+
+
+
+
+        
+        
+       
+
+ 
+
+     
+
          
 
