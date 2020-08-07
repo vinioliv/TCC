392359ca16d2c0e712e8756ec5fe7847155d16eb
Samuel Rodrigues <42328238+Mkr165@users.noreply.github.com>
2020-08-06 23:12:18 -0300

    Update routes.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,16 +1,63 @@
+// import { createAppContainer } from 'react-navigation';
+// import { createStackNavigator} from 'react-navigation-stack';
+
+// import SignIn from './pages/signIn';
+// // import SignUp from './pages/signUp';
+// import Main from './pages/main';
+
+// const Routes = createStackNavigator({
+// SignIn,
+// // SignUp,
+// Main
+// });
+
+// const RoutesCtn = createAppContainer(Routes);
+
+// export default RoutesCtn;
+
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator} from 'react-navigation-stack';
+ 
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-import SignIn from './pages/signIn';
-// import SignUp from './pages/signUp';
-import Main from './pages/main';
 
-const Routes = createStackNavigator({
-SignIn,
-// SignUp,
-Main
-});
+import HomeScreen from './pages/main';
+import MypageScreen from './pages/signIn';
+ 
 
-const RoutesCtn = createAppContainer(Routes);
+const MainTabs = createBottomTabNavigator({
+    HOME: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarLabel: 'HOME',
+      },
+    },
+    Buscar: {
+      screen: MypageScreen,
+      navigationOptions: {
+        tabBarLabel: 'Buscar',
+      },
+    },
+    Carrinho: {
+        screen: MypageScreen,
+        navigationOptions: {
+          tabBarLabel: 'Carrinho',
+        },
+      },
+    Perfil: {
+      screen: MypageScreen,
+      navigationOptions: {
+        tabBarLabel: 'Perfil',
+      },
+    },
+    Menu: {
+        screen: MypageScreen,
+        navigationOptions: {
+          tabBarLabel: 'Menu',
+        },
+      },
+  });
+ const RoutesCtn = createAppContainer(MainTabs);
 
-export default RoutesCtn;
+ export default RoutesCtn;
+ 
