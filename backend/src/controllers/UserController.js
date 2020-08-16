96d86a46025a582ccf210387784c22af02090e09
Vinicius Oliveira <vinicius.oliveira148571@gmail.com>
2020-08-16 01:23:49 -0300

    Organização do backend

---
+++ backend/src/controllers/UserController.js
@@ -0,0 +1,30 @@
+const User  = require('../models/User.js')
+
+module.exports = {
+
+    async createuser(request, response){
+    const{nome, email, senha, dtnascimento, tipo} = request.body;
+    const img = "default.png"
+    const find = await User.findOne({
+      where: {
+      nm_email: email
+      }
+    })
+
+      if(find == null){
+      await User.create({
+      nm_usuario: nome, nm_email: email, nm_senha: senha,
+      dt_nascimento: dtnascimento, cd_tipo_usuario: tipo,
+      nm_img: img
+     }).then(newuser => {
+      return response.status(200).json({msg: "Cadastro Realizado com sucesso!", id: newuser.cd_usuario })
+        });   
+      }
+    else return response.status(422).json("E-mail Já cadastrado")
+    }
+}
+   
+
+
+
+   
