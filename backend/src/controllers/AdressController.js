e97e34aaddb3a692c9d5e6ad98bcd3d7f9d564ab
Vinicius Oliveira <vinicius.oliveira148571@gmail.com>
2020-08-30 21:36:18 -0300

    cadastro de endereço

--- backend/src/controllers/AdressController.js
+++ backend/src/controllers/AdressController.js
@@ -1,15 +1,25 @@
-const Adress = require('../models/Adress')
+const Adress = require("../models/Adress");
 
 module.exports = {
-async createadress(request, response){
+  async createadress(request, response) {
     const user_id = request.headers.authorization;
-    const{cep, uf, cidade, rua, casa, complement} = request.body;
+    const { cep, uf, cidade, rua, casa, complement } = request.body;
 
     await Adress.create({
-        cd_usuario: user_id, cd_cep: cep, sg_estado: uf,
-        nm_cidade: cidade, nm_rua: rua, nr_casa: casa, ds_complemento: complement
-      }).then(newadress => {
-        return response.status(200).json({msg: "Endereço cadastrado com sucesso", id_adress: newadress.id_endereco})
-    })
-  }
-}
+      cd_usuario: user_id,
+      cd_cep: cep,
+      sg_estado: uf,
+      nm_cidade: cidade,
+      nm_rua: rua,
+      nr_casa: casa,
+      ds_complemento: complement,
+    }).then((newadress) => {
+      return response
+        .status(200)
+        .json({
+          msg: "Endereço cadastrado com sucesso",
+          id_adress: newadress.id_endereco,
+        });
+    });
+  },
+};
